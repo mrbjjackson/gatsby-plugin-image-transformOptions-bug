12db8e12e31dc2b85ede4ff0d883f6888bc64e67
mrbjjackson <mrbjjackson@googlemail.com>
2022-03-04 10:49:54 +0000

    added commented alternate graphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,12 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 // markup
 const IndexPage = ({ data }) => {
-   const image1 = getImage(data.duotone1.nodes[1]);
-   const image2 = getImage(data.duotone2.nodes[1]);
+   const image1 = getImage(
+      data.duotone1.nodes.find((img) => img.fluid.originalName === "test.jpg")
+   );
+   const image2 = getImage(
+      data.duotone2.nodes.find((img) => img.fluid.originalName === "test.jpg")
+   );
    return (
       <main>
          <h1>Hello</h1>
@@ -21,9 +25,11 @@ export const pageQuery = graphql`
    query MyQuery {
       duotone1: allImageSharp {
          nodes {
+            fluid {
+               originalName
+            }
             gatsbyImageData(
                width: 500
-               quality: 81
                transformOptions: {
                   duotone: { highlight: "#BBFFE6", shadow: "#51758D" }
                }
@@ -32,9 +38,11 @@ export const pageQuery = graphql`
       }
       duotone2: allImageSharp {
          nodes {
+            fluid {
+               originalName
+            }
             gatsbyImageData(
                width: 500
-               quality: 80
                transformOptions: {
                   duotone: { highlight: "#F1D283", shadow: "#000000" }
                }
@@ -43,3 +51,36 @@ export const pageQuery = graphql`
       }
    }
 `;
+
+// export const pageQuery = graphql`
+//    query MyQuery {
+//       duotone1: allImageSharp {
+//          nodes {
+//             fluid {
+//                originalName
+//             }
+//             gatsbyImageData(
+//                width: 500
+//                quality: 81
+//                transformOptions: {
+//                   duotone: { highlight: "#BBFFE6", shadow: "#51758D" }
+//                }
+//             )
+//          }
+//       }
+//       duotone2: allImageSharp {
+//          nodes {
+//             fluid {
+//                originalName
+//             }
+//             gatsbyImageData(
+//                width: 500
+//                quality: 80
+//                transformOptions: {
+//                   duotone: { highlight: "#F1D283", shadow: "#000000" }
+//                }
+//             )
+//          }
+//       }
+//    }
+// `;
